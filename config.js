1ab9bdeaab5cd914996aa75353ea5da491080b90
swissbutter <mutuldu@naver.com>
2026-02-15 16:25:37 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Supabase 환경 설정
+window.SAGAK_CONFIG = {
+    SUPABASE_URL: 'https://ynwykcmayqadxcajayzn.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlud3lrY21heXFhZHhjYWpheXpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwNjk4ODEsImV4cCI6MjA4NjY0NTg4MX0.N4RzHTymRmPp-v9lHy-u3ZSevDNlTmjqqOM_Vlh8Lno'
+};
